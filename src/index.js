65632eff4ca4d425fed6d74e20de9fb93e85c71b
Abid1404 <aabidshaikh14.04@gmail.com>
2024-10-13 12:12:14 +0530

    Async Handling

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Srever is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=> {
+    console.log("MONGO db connection failed", err )
+})
 // APPROCH 1
 /*
 // type 1 of connection 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,35 @@
+// with the help of promise
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+            Promise.resolve(requestHandler(req,res,next)).
+            catch((err) => next(err))
+    }
+
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+// ye with the help of try catch tha 
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     }
+//     catch (error){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+
+//     }
+
+// }
+
+// export {asyncHandler}
